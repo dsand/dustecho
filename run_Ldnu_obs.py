94dccb97cc1624fa76a40bb595d8f6aa5ff76e5b
dsand <dave.j.sand@gmail.com>
2022-12-28 13:21:58 -0700

    Add example notebook, add JWST filters and angles

--- run_Ldnu_obs.py
+++ run_Ldnu_obs.py
@@ -3,8 +3,8 @@ from math import pi
 from fpath import *
 
 nH0_list = [1]        # [cm^-3]
-lamobs_list = [1.14, 1.39, 1.54]    # micron
-theobs_list = [20*pi/180]   # radian
+lamobs_list = [1.14, 1.39, 1.54, 0.90, 1.15, 1.5, 2.0, 2.77, 3.56, 4.44]    # micron
+theobs_list = [20*pi/180,30*pi/180.]   # radian
 
 
 for i in range(len(nH0_list)):
